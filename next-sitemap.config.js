bba890f091e4e1cd9de7b627c0df73f68e20ebcf
KanekiYuto <kaneki.yuto.404@gmail.com>
2026-01-27 17:53:48 +0800

    Add next-sitemap configuration and update build script to generate sitemap. Include robots.txt and sitemap.xml in .gitignore.

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [{ userAgent: "*", allow: "/" }],
+  },
+};
+
