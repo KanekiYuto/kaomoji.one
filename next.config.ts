9b8842b20f78aff9eef558264287f7dd9d892ba6
KanekiYuto <kaneki.yuto.404@gmail.com>
2026-02-21 12:32:49 +0800

    Implement redirects in next.config.ts to route www.kaomoji.one to kaomoji.one; remove obsolete middleware for streamlined request handling.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.kaomoji.one",
+          },
+        ],
+        destination: "https://kaomoji.one/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
